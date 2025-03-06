5f4e88c2e37440b124bf38857ac61666c76d3555
felixmiranda1 <felix.miranda1@outlook.com>
2025-03-06 18:50:15 -0300

    Inserting django csp for maze

--- alugAE/settings.py
+++ alugAE/settings.py
@@ -70,7 +70,17 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware', 
+    "csp.middleware.CSPMiddleware", 
 ]
+
+# Permitir o Maze como fonte confiável
+CSP_DEFAULT_SRC = ["'self'", "https://*.maze.co"]
+CSP_SCRIPT_SRC = ["'self'", "https://snippet.maze.co"]
+CSP_FONT_SRC = ["'self'", "https://snippet.maze.co"]
+CSP_STYLE_SRC = ["'self'", "https://snippet.maze.co"]
+CSP_IMG_SRC = ["'self'", "https://snippet.maze.co"]
+CSP_CONNECT_SRC = ["'self'", "https://api.maze.co", "https://prompts.maze.co"]
+CSP_FRAME_SRC = ["'self'", "https://t.maze.co"]
 
 ROOT_URLCONF = 'alugAE.urls'
 
